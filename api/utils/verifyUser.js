3e523e7dae3c2e3e784cf3064cc98dc8cb74501f
roshank07 <roshann.purbey@gmail.com>
2024-03-14 09:22:57 +0545

    debugging cors issue-5

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -5,12 +5,10 @@ export const verifyUser = async (req, res, next) => {
   // console.log("inhere");
   const token = req.cookies.access_token;
   if (!token) {
-    console.log("1", token);
     return next(errorHandler(401, "Unauthorized 1"));
   }
-  jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+  jwt.verify(token, process.env.JWT_secret, (error, user) => {
     if (error) {
-      console.log("2", error);
       return next(errorHandler(401, "Unauthorized 2"));
     } else {
       req.user = user;
