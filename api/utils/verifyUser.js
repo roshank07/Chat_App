3bf0001cd3138be2e16433cc26cfa8c8ddaa47ba
roshank07 <roshann.purbey@gmail.com>
2024-03-14 09:16:19 +0545

    debugging cors issue-4

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -5,12 +5,13 @@ export const verifyUser = async (req, res, next) => {
   // console.log("inhere");
   const token = req.cookies.access_token;
   if (!token) {
-    // console.log('inhere2');
-    return next(errorHandler(401, "Unauthorized"));
+    console.log("1", token);
+    return next(errorHandler(401, "Unauthorized 1"));
   }
   jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
     if (error) {
-      return next(errorHandler(401, "Unauthorized"));
+      console.log("2", error);
+      return next(errorHandler(401, "Unauthorized 2"));
     } else {
       req.user = user;
       next();
